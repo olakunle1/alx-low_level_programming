05ff0209741b3052ce2a28709316f34061f34b86
olakunle1 <kuzzle4real@gmail.com>
2022-04-07 11:47:47 -0700

    y

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
-   * main - Entry point
-    *
-     * Return: 0 after printing the function
-      */
+* main - Entry point
+
+* Return: 0 after printing the function
+*/
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

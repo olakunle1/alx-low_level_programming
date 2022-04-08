3cff6e7a11cc824daac4e484853ada0925791690
olakunle1 <kuzzle4real@gmail.com>
2022-04-08 13:18:00 -0700

    u

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,10 @@ int main(void)
 	   *    * variable n has been declared
 	   *       * coding then assigns the random value.
 	   *          */
+
+
 		int n;
+
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;

966258872da7fa38d4bab537325b669b1ad92611
olakunle1 <kuzzle4real@gmail.com>
2022-04-08 13:42:53 -0700

    u

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,10 @@
      *
      * Description: Prints last digit of random number and tells
        * if it is greater than 5, 0, or less than 6 and not 0.
-        *
+        
   * Return: 0 for success
   */
+
 int main(void)
 {
 

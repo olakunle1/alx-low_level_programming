ceb8d8aab3ae8f0a7899329a4d81b918fcc528ad
olakunle1 <kuzzle4real@gmail.com>
2022-04-09 04:34:54 -0700

    u

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,77 +1,25 @@
-#include <stdlib.h>
-
-#include <stdio.h>
-
-#include <time.h>
-
-
+#!include <stdlib.h>
+#!include <time.h>
+#!include <stdio.h>
 
 /**
- *
- *  main - The last digit
- *
- *   @void: Empty parameter list for main.
- *
- *    
- *
- *      Description: Prints last digit of random number and tells
- *
- *       if it is greater than 5, 0, or less than 6 and not 0.
- *
- *        
- *
- *         Return: 0 for success
- *
-  */
-
-int main(void)
-
+ * main - prints the last digi of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main (void)
 {
+	int n;
 
-
-
-		int n;
-
-
-
-			srand(time(0));
-
-
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					printf("Last digit of %d is %d ", n, (n % 10));
-
-
-
-						if ((n % 10) > 5)
-
-								{
-
-											printf("and is greater than 5\n");
-
-												}
-
-							else if ((n % 10) == 0)
-
-									{
-
-												printf("and is 0\n");
-
-													}
-
-								else
-
-										{
-
-													printf("and is less than 6 and not 0\n");
-
-														}
-
-
-
-									return (0);
-
+		strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digi of %d id %d ", n, n % 10);
+	if (n % 10 < 5)
+		print("and id greater than 5/n");
+	else if (n % 10 == 0)
+		printf("and is 0/n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0/m");
+	return (0);
 }
+

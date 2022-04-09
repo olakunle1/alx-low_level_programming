77a15fe7679b2d83992007ddca27f57dfaa34a71
olakunle1 <kuzzle4real@gmail.com>
2022-04-08 23:28:42 -0700

    u

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,33 @@
-#!include <stdlib.h>
-#!include <stdio.h>
-#!include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 /**
- * main - Prints alphabet
- * @vpoid: Empty parameter list for main.
- *
- * Description: Prints the alphabet in lower and upper case.
- * Can only use putcher
- *
- * Return: 0 for success
- */
+   * main - Prints alphabet
+    * @void: Empty parameter list for main.
+     *
+      * Description: Prints the alphabet in lower and upper case
+       * Can only use putchar
+        *
+         * Return: 0 for success
+         */
 int main(void)
 {
-	char lettr = 'a';
-	while (letter <= 'z')
-	{
+		char letter = 'a';
 
+			while (letter <= 'z')
+					{
+								putchar(letter);
+										letter++;
+											}
 
-		putchar(letter);
-		letter++;
-	}
+				letter = 'A';
 
-
-	letter = 'A';
-	while (letter <= 'Z')
-	
-	{
-		putchar(letter);
-	
-		letter++;
-	}
-	putchar('/n');
-	return (0);
+					while (letter <= 'Z')
+							{
+										putchar(letter);
+												letter++;
+													}
+						putchar('\n');
+							return (0);
 }
-		

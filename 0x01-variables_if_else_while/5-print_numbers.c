49ef9474d8056834b6bbe1c06d94f01abb4df40c
olakunle1 <kuzzle4real@gmail.com>
2022-04-10 07:31:39 -0700

    u

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,14 @@
  * Return: 0
  */
 int main(void)
-{
-	char c = '0';
+	{
+		char c = '0';
 
-	while (c <= '9')
- 	{
-		putchar(c);
-		c++;
+		while (c <= '9')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
-}

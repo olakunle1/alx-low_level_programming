da9d30d159ab3faa5a745c9cc0c6e80bc0ce13c8
olakunle1 <kuzzle4real@gmail.com>
2022-04-10 06:26:40 -0700

    u

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,32 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-
 /**
-  * main - Prints base 16
-    * @void: Empty parameter list for main.
-     *
-      * Description: Prints all hexadecimals
-       *
-        * Return: 0 for success
-        */
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int num = 48;
-
-			while (num <= 57)
-					{
-								putchar(num);
-										num++;
-											}
-
-				num = 97;
-
-					while (num <= 102)
-							{
-										putchar(num);
-												num++;
-													}
-						putchar('\n');
-							return (0);
+		char ch;
+		
+		for (ch = 'z'; ch >= 'a'; ch--)
+			{
+			putchar(ch);
+			}
+			putchar('\n');
+			return (0);
 }

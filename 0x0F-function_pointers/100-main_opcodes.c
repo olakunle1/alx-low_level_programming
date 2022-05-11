870287a4605975b33c92b104ae4f6bd4766513f8
olakunle1 <kuzzle4real@gmail.com>
2022-05-11 12:52:50 -0700

    j

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,23 +2,33 @@
 #include <stdlib.h>
 
 /**
- * main - searchs for an integer
- *@argc: number of arguments
- *@argv: array with arguments
+ * main - program that prints the opcodes of its own main function
  *
- * Return: no return
+ * @argc: count of args
+ * @argv: array of args
+ *
+ * Return: int
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
+	int i, bytes;
+	char *addr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	addr = (char *)main;
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", addr[i]);
+	printf("%02hhx\n", addr[i]);
 	return (0);
 }

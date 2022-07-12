3510fe28bb42ba8efeeac7bc59f084fd4eaf8925
olakunle1 <101416230+olakunle1@users.noreply.github.com>
2022-07-12 22:40:01 +0100

    Create 0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * Auth: Agbiawoei Michael Ifiekemi
+ */
+
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
